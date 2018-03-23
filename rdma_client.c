1579682fae081e18dab54b50ccd791d29d6547f8
Your Name <you@example.com>
2018-03-23 18:26:01 +0800

    fast

--- rdma_client.c
+++ rdma_client.c
@@ -177,7 +177,7 @@ static int client_prepare_connection(struct sockaddr_in *s_addr)
 	qp_init_attr.cap.max_recv_sge = MAX_SGE; /* Maximum SGE per receive posting */
 	qp_init_attr.cap.max_recv_wr = 1; /* Maximum receive posting capacity */
 	qp_init_attr.cap.max_send_sge = MAX_SGE; /* Maximum SGE per send posting */
-	qp_init_attr.cap.max_send_wr = 20000; /* Maximum send posting capacity */
+	qp_init_attr.cap.max_send_wr = 15000; /* Maximum send posting capacity */
 	qp_init_attr.qp_type = IBV_QPT_RC; /* QP type, RC = Reliable connection */
 	/* We use same completion queue, but one can use different queues */
 	qp_init_attr.recv_cq = client_cq; /* Where should I notify for receive completion operations */
