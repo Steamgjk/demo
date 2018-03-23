caf7eed60481765b6aa73da6a1cf92f7412fed5d
Your Name <you@example.com>
2018-03-23 10:51:12 +0800

    server

--- rdma_client.c
+++ rdma_client.c
@@ -583,7 +583,7 @@ int main(int argc, char **argv)
 	}
 	**/
 	server_sockaddr.sin_port = htons(DEFAULT_RDMA_PORT);
-	get_addr("12.12.10.16", server_sockaddr)
+	get_addr("12.12.10.16", (struct sockaddr*) &server_sockaddr)
 	src = calloc(strlen(int) , 1);
 
 
