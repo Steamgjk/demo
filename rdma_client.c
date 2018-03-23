d8288ccb7b850992bfefee29eb2b75c41c8a7e6c
Your Name <you@example.com>
2018-03-23 11:36:03 +0800

    sizeof

--- rdma_client.c
+++ rdma_client.c
@@ -589,7 +589,9 @@ int main(int argc, char **argv)
 	get_addr("12.12.10.16", (struct sockaddr*) &server_sockaddr);
 	server_sockaddr.sin_port = htons(DEFAULT_RDMA_PORT);
 	src = calloc(sizeof(int) , 1);
-
+	dst = calloc(sizeof(int), 1);
+	int curR = 1;
+	memcpy(src, &curR, sizeof(1));
 
 	ret = client_prepare_connection(&server_sockaddr);
 	if (ret)
