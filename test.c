e77ccb31dbf20536061b3a3456f4cb417b75d285
Your Name <you@example.com>
2018-03-25 16:10:20 +0800

    test

--- test.c
+++ test.c
@@ -6,13 +6,22 @@ int main()
 	a = 0;
 	b = 1;
 	c = 3;
+	char* tt = calloc(sizeof(int), 1);
+	int* tmp_int = (int*)(void*)tt;
 	int t = 10000;
 	long long L1, L2;
 	struct timeval tv;
+	int cnt = 0;
 	L1 = tv.tv_sec * 1000 * 1000 + tv.tv_usec;
-	while (a < t)
+
+	while (1 == 1)
 	{
-		a = a + b;
+		cnt++;
+		*tmp_int = (int)cnt;
+		if (cnt >= 10000)
+		{
+			break;
+		}
 	}
 	L2 = tv.tv_sec * 1000 * 1000 + tv.tv_usec;
 	printf("%d ops  duration =  %lld  micro seconds \n", (a), L2 - L1);
