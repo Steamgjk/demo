253956550f7d3cd1546e722ef9dd8b11e386e740
Your Name <you@example.com>
2018-03-26 12:53:26 +0800

    test

--- test.c
+++ test.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <sys/time.h>
+#include <stdlib.h>
 int main()
 {
 	int a, b, c;
 	a = 0;
 	b = 1;
 	c = 3;
-	char* tt = calloc(sizeof(int), 5);
-	int arr[5] = {0, 1, 2, 3, 4};
-	int* tmp_int = (int*)(void*)tt;
+
 	int t = 10000;
 	long long L1, L2;
 	struct timeval tv;
 	int cnt = 0;
 	L1 = tv.tv_sec * 1000 * 1000 + tv.tv_usec;
-
-	while (cnt <= 10000)
+	float cap = 100;
+	float arr[cap];
+	int i = 0;
+	for (i = 0; i < cap; i++)
+	{
+		arr[i] = i;
+	}
+	while (cnt <= t)
 	{
 		cnt++;
-		memcpy(tmp_int, arr, sizeof(int) * 5);
+		float* tt = calloc(sizeof(float), cap);
+		memcpy(tt, arr, sizeof(float) * cap);
+		for (i = 0; i < cap; i++)
+		{
+			arr[i] = arr[i] + tt[i];
+		}
+		free(tt);
 	}
 	L2 = tv.tv_sec * 1000 * 1000 + tv.tv_usec;
 	printf("%d ops  duration =  %lld  micro seconds \n", (cnt), L2 - L1);
