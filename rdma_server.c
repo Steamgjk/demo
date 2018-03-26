373c179bf73412e7874a83e0bca2db89faa2e5e4
Your Name <you@example.com>
2018-03-26 14:38:15 +0800

    test

--- rdma_server.c
+++ rdma_server.c
@@ -365,7 +365,7 @@ static int send_server_metadata_to_client()
 	debug("tmp_cnt=%d\n", *tmp_cnt );
 	// Send WR to client.
 	ret = ibv_post_send(client_qp, &server_send_wr, &bad_wr);
-	printf("After  post send  to sleep\n");
+	debug("After  post send  to sleep\n");
 	int i = 0;
 	long long L1, L2;
 	struct timeval tv;
@@ -374,6 +374,7 @@ static int send_server_metadata_to_client()
 		debug("remote write?  %d", *tmp_cnt);
 		sleep(1);
 	}
+	debug("hahah");
 	gettimeofday(&tv, NULL);
 	L1 = tv.tv_sec * 1000 * 1000 + tv.tv_usec;
 	while ((*tmp_cnt) < 5000)
