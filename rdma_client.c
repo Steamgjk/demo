4285c11c6081381841b5b5f326d46d2cba018a29
Your Name <you@example.com>
2018-05-18 15:47:18 +0800

    more sz

--- rdma_client.c
+++ rdma_client.c
@@ -383,7 +383,9 @@ static int client_remote_memory_ops()
 		for (int i = 0; i < ele_num; i++)
 		{
 			d_data[i] = drand48();
+			printf("%lf\n", d_data[i] );
 		}
+		printf("\n");
 		char* buf = (void*) tmp_int;
 		memcpy(buf + sizeof(int), d_data, data_sz);
 		printf("cnt=%d *src =%d\n",  cnt, *((int*)((void*)src)) );
